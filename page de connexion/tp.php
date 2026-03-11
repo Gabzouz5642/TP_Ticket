a3afb7aee4780826bf4ab114f570e46d4f54d7ea
Gabzouz5642 <gabindalibard@gmail.com>
2026-03-11 10:27:51 +0100

    Add files via upload

---
+++ page de connexion/tp.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="tp.css">
+    <title>ESIEA</title>
+    <style>
+    </style>
+</head>
+<body>
+        
+    <div class="login-box">
+        <h2>Connexion</h2>
+            <input type="text" name="username" placeholder="Nom d'utilisateur" required>
+            <input type="password" name="password" placeholder="Mot de passe" required>
+            <button type="submit" onclick="window.location.href='../tableau de bord/dashboard.php'">Se connecter</button>
+    </div>
+    <script src="tp.js"></script>
+
+</body>
+</html>
